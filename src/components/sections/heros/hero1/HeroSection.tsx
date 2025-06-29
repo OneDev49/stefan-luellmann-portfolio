7010498674dbef544a9d98bffa6a93bd25abe6b4
OneDev49 <dev49.sgra@protonmail.com>
2025-06-29 15:50:04 +0200

    refactor: add clsx for cleaner classes

--- src/components/sections/heros/hero1/HeroSection.tsx
+++ src/components/sections/heros/hero1/HeroSection.tsx
@@ -1,5 +1,6 @@
 import styles from './HeroSection.module.scss';
 import GradientButton from '@/components/ui/buttons/gradientbutton/GradientButton';
+import clsx from 'clsx';
 
 
 
@@ -16,11 +17,22 @@ interface HeroProps {
 
 export default function HeroSection({ heading, paragraph, gradientButton: {href, title, variant} }: HeroProps) {
     
+    const contentClassNames = clsx(
+        styles.Content,
+        'nwt--flex-c-c-n',
+        'nwt--txt-center',
+    );
+    const contentInnerClassName = clsx(
+        styles.ContentInner,
+        'nwt--flex-c-n-col',
+        'nwt--txt-center',
+    );
+
     return (
         <section className={styles.nwtHeroWrapper}>
-            <div className={`${styles.Content} nwt--flex-c-c-n nwt--text-center`}>
-                <div className={`${styles.ContentInner} nwt--flex-c-n-col nwt--txt-center`}>
-                    <h1 className={`nwt--f-h1`}>
+            <div className={`${contentClassNames}`}>
+                <div className={`${contentInnerClassName}`}>
+                    <h1 className="nwt--f-h1">
                         {heading}
                     </h1>
                     <p>
