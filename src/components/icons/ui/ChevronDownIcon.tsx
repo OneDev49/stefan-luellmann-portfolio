7ca1f3499b9de8bef5d885cd0b2e16500275cedc
OneDev49 <dev49.sgra@protonmail.com>
2025-06-29 17:42:49 +0200

    feat(icons): create directional chevron icon components

---
+++ src/components/icons/ui/ChevronDownIcon.tsx
@@ -0,0 +1,55 @@
+
+/**
+ * @license
+ * SVG Code by:
+ * Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com
+ * License - https://fontawesome.com/license/free
+ * Copyright 2025 Fonticons, Inc.
+ * 
+ * For more information, check the LICENSE.txt of the Repository
+ */
+
+
+const iconVariants = {
+    solid: {
+        viewBox: '0 0 512 512',
+        path: 'M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z',
+    },
+};
+
+
+type IconVariant = keyof typeof iconVariants;
+
+
+interface IconProps {
+    width?: number | string;
+    height?: number | string;
+    className?: string;
+    color?: string;
+    variant?: IconVariant;
+}
+
+
+export default function ChevronDownIcon({ 
+        width = 24, 
+        height = 24, 
+        className, 
+        variant = 'solid', 
+        color 
+    }: IconProps) {
+    
+    const selectedVariant = iconVariants[variant];
+
+    return (
+        <svg 
+            xmlns="http://www.w3.org/2000/svg" 
+            viewBox={selectedVariant.viewBox}
+            width={width} 
+            height={height}
+            fill={color || 'currentColor'}
+            className={className}
+        >
+            <path d={selectedVariant.path} />
+        </svg>
+    )
+}
