25010a9eec9e184e4be547627e3b3168ec99ea8e
OneDev49 <dev49.sgra@protonmail.com>
2025-07-04 18:11:38 +0200

    feat(map): add new tech icons to TechIconMap

--- src/components/icons/TechIconMap.tsx
+++ src/components/icons/TechIconMap.tsx
@@ -1,10 +1,20 @@
 import NextJSIcon from "./tech/NextJSIcon";
+import ReactIcon from "./tech/ReactIcon";
+import TypeScriptIcon from "./tech/TypeScriptIcon";
+import SASSIcon from "./tech/SASSIcon";
+import MDXIcon from "./tech/MDXIcon";
+import VercelIcon from "./tech/VercelIcon";
 
 
-export type TechnologyName = 'nextjs';
+export type TechnologyName = 'nextjs' | 'react' | 'typescript' | 'sass' | 'mdx' | 'vercel';
 
 export const TechIconMap: Record<TechnologyName, React.FC<React.SVGProps<SVGSVGElement>>> = {
     nextjs: NextJSIcon,
+    react: ReactIcon,
+    typescript: TypeScriptIcon,
+    sass: SASSIcon,
+    mdx: MDXIcon,
+    vercel: VercelIcon,
 }
 
 interface TechIconProps extends React.SVGProps<SVGSVGElement> {
