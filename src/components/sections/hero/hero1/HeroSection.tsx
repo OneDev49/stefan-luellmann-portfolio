fb2bf447d71fa66fc9bb1ac80729d29479994187
OneDev49 <dev49.sgra@protonmail.com>
2025-07-06 23:10:05 +0200

    feat: add option for more buttons and subheading

--- src/components/sections/hero/hero1/HeroSection.tsx
+++ src/components/sections/hero/hero1/HeroSection.tsx
@@ -1,44 +1,53 @@
 import styles from './HeroSection.module.scss';
-import GradientButton, {GradientButtonProps} from '@/components/ui/buttons/gradientbutton/GradientButton';
+import GradientButton, {
+  GradientButtonProps,
+} from '@/components/ui/buttons/gradientbutton/GradientButton';
 import clsx from 'clsx';
 
-
-
 interface HeroProps {
-    heading?: string;
-    paragraph?: string;
-    gradientButton: GradientButtonProps;
+  heading?: string;
+  subheading?: string;
+  paragraph?: string;
+  gradientButton: GradientButtonProps[];
 }
 
+export default function HeroSection({
+  heading,
+  subheading,
+  paragraph,
+  gradientButton,
+}: HeroProps) {
+  const contentClassNames = clsx(
+    styles.content,
+    'nwt--flex-c-c-n',
+    'nwt--txt-center'
+  );
+  const contentInnerClassName = clsx(
+    styles.contentInner,
+    'nwt--flex-c-n-col',
+    'nwt--txt-center'
+  );
 
-export default function HeroSection({ heading, paragraph, gradientButton }: HeroProps) {
-    
-    const contentClassNames = clsx(
-        styles.Content,
-        'nwt--flex-c-c-n',
-        'nwt--txt-center',
-    );
-    const contentInnerClassName = clsx(
-        styles.ContentInner,
-        'nwt--flex-c-n-col',
-        'nwt--txt-center',
-    );
-
-    return (
-        <section className={styles.nwtHeroWrapper}>
-            <div className={`${contentClassNames}`}>
-                <div className={`${contentInnerClassName}`}>
-                    <h1 className="nwt--f-h1">
-                        {heading}
-                    </h1>
-                    <p>
-                        {paragraph}
-                    </p>
-                    <GradientButton {...gradientButton}/>
-                </div>
-
-            </div>
-
-        </section>
-    );
+  return (
+    <section className={styles.wrapper}>
+      <div className={`${contentClassNames}`}>
+        <div className={`${contentInnerClassName}`}>
+          <div>
+            <h1 className='nwt--f-h1'>{heading}</h1>
+            <h2 className='nwt--f-h2'>{subheading}</h2>
+          </div>
+          <p>{paragraph}</p>
+          <div className='nwt--flex-c-n-n'>
+            {gradientButton.map((button, index) => (
+              <GradientButton
+                key={index}
+                href={button.href}
+                children={button.children}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
