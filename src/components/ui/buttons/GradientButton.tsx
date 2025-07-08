b5203162fa1312c33336bed81e9eea845e5ae47d
OneDev49 <dev49.sgra@protonmail.com>
2025-07-08 20:24:11 +0200

    feat(prop): create rel prop for GradientButton

--- src/components/ui/buttons/GradientButton.tsx
+++ src/components/ui/buttons/GradientButton.tsx
@@ -3,39 +3,42 @@ import clsx from 'clsx';
 import styles from './GradientButton.module.scss';
 import React from 'react';
 
-
-
 export interface GradientButtonProps {
-    href: string;
-    children: React.ReactNode;
-    target?: string;
-    variant?: 'blue' | 'orange' | 'green';
-    className?: string;
+  href: string;
+  children: React.ReactNode;
+  rel?: string;
+  target?: string;
+  variant?: 'blue' | 'orange' | 'green';
+  className?: string;
 }
 
-
-export default function GradientButton({ 
-    href, 
-    children,
-    target,
-    variant = 'blue',
+export default function GradientButton({
+  href,
+  children,
+  rel,
+  target,
+  variant = 'blue',
+  className,
+}: GradientButtonProps) {
+  const buttonClassNames = clsx(
+    styles.nwtGradientButton,
+    {
+      [styles.nwtBlueGradient]: variant == 'blue',
+      [styles.nwtOrangeGradient]: variant == 'orange',
+      [styles.nwtGreenGradient]: variant == 'green',
+    },
+    'nwt--flex-c-c-n',
     className
-    }: GradientButtonProps) {
+  );
 
-        const buttonClassNames = clsx(
-            styles.nwtGradientButton,
-            {
-                [styles.nwtBlueGradient]: variant == 'blue',
-                [styles.nwtOrangeGradient]: variant == 'orange',
-                [styles.nwtGreenGradient]: variant == 'green',
-            },
-            'nwt--flex-c-c-n',
-            className
-        );
-
-        return (
-            <Link className={`${buttonClassNames} `} href={href} target={target}>
-                {children}
-            </Link>
-        );
+  return (
+    <Link
+      className={`${buttonClassNames} `}
+      href={href}
+      rel={rel}
+      target={target}
+    >
+      {children}
+    </Link>
+  );
 }
