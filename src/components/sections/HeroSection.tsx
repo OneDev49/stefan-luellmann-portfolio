f279adf50fe889c592ab30f1af607b4e14caceb1
OneDev49 <dev49.sgra@protonmail.com>
2025-07-12 23:22:18 +0200

    feat(component): create new variant prop and add new variant to HeroSection

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -4,12 +4,21 @@ import clsx from 'clsx';
 import MultiThinWaves from '../effects/MultiThinWave';
 import { ComponentProps } from 'react';
 
+interface ImageProps {
+  src: string;
+  alt: string;
+  width: string;
+  height: string;
+}
+
 interface HeroProps {
   heading?: string;
   subheading?: string;
   multiThinWave?: 0 | 1;
   paragraph?: string;
   gradientButton: SingleButtonProps[];
+  variant?: 'singleColumn' | 'doubleColumn';
+  doubleColumnImage?: ImageProps;
 }
 
 type SingleButtonProps = ComponentProps<typeof GradientButton>;
@@ -20,40 +29,86 @@ export default function HeroSection({
   multiThinWave = 0,
   paragraph,
   gradientButton,
+  variant = 'singleColumn',
+  doubleColumnImage,
 }: HeroProps) {
-  const contentClassNames = clsx(
-    styles.content,
-    'nwt--flex-c-c-n',
-    'nwt--txt-center'
-  );
-  const contentInnerClassName = clsx(
-    styles.contentInner,
-    'nwt--flex-c-n-col',
-    'nwt--txt-center'
-  );
+  const contentClassNames = clsx(styles.content, 'nwt--flex-c-n-n');
 
-  return (
-    <section className={styles.wrapper}>
-      {multiThinWave === 1 && (
-        <div className={styles.sectionMultiThinWave}>
-          <MultiThinWaves />
-        </div>
-      )}
-
-      <div className={`${contentClassNames}`}>
-        <div className={`${contentInnerClassName}`}>
-          <div>
-            <h1 className='nwt--f-h1'>{heading}</h1>
-            <h2 className='nwt--f-h2'>{subheading}</h2>
+  if (variant === 'singleColumn') {
+    const contentInnerClassName = clsx(
+      styles.contentInner,
+      'nwt--flex-c-n-col'
+    );
+    return (
+      <section className={styles.wrapper} data-type='single'>
+        {multiThinWave === 1 && (
+          <div className={styles.sectionMultiThinWave}>
+            <MultiThinWaves />
           </div>
-          <p>{paragraph}</p>
-          <div className='nwt--flex-c-n-n'>
-            {gradientButton.map((button, index) => (
-              <GradientButton key={index} {...button} />
-            ))}
+        )}
+
+        <div className={`${contentClassNames}`}>
+          <div className={`${contentInnerClassName}`}>
+            <div>
+              <h1 className='nwt--f-h1'>{heading}</h1>
+              <h2 className='nwt--f-h2'>{subheading}</h2>
+            </div>
+            <p>{paragraph}</p>
+            <div className='nwt--flex-c-n-n'>
+              {gradientButton.map((button, index) => (
+                <GradientButton key={index} {...button} />
+              ))}
+            </div>
           </div>
         </div>
-      </div>
-    </section>
-  );
+      </section>
+    );
+  }
+
+  if (variant === 'doubleColumn') {
+    const contentInnerClassName = clsx(styles.contentInner, 'nwt--flex-c-n-n');
+
+    const contentDoubleLeftClassName = clsx(
+      styles.doubleLeft,
+      'nwt--flex-n-n-col'
+    );
+    return (
+      <section className={styles.wrapper} data-type='double'>
+        {multiThinWave === 1 && (
+          <div className={styles.sectionMultiThinWave}>
+            <MultiThinWaves />
+          </div>
+        )}
+
+        <div className={contentClassNames}>
+          <div className={contentInnerClassName}>
+            <div className={contentDoubleLeftClassName}>
+              <div>
+                <h1 className='nwt--f-h1'>{heading}</h1>
+                <h2 className='nwt--f-h3'>{subheading}</h2>
+              </div>
+              {paragraph}
+              <div className='nwt--flex-c-n-n'>
+                {gradientButton.map((button, index) => (
+                  <GradientButton key={index} {...button} />
+                ))}
+              </div>
+            </div>
+            <div className={styles.doubleRight}>
+              {doubleColumnImage && (
+                <img
+                  loading='eager'
+                  draggable='false'
+                  src={doubleColumnImage.src}
+                  alt={doubleColumnImage.alt}
+                  height={doubleColumnImage.height}
+                  width={doubleColumnImage.width}
+                />
+              )}
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
