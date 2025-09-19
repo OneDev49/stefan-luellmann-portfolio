6f40aa0de1297ce4295b845965a5ee5ad7a84606
OneDev49 <dev49.sgra@protonmail.com>
2025-09-19 23:34:27 +0200

    feat(mainpage): create new sections for mainpage

---
+++ src/app/_components/ProjectShowcaseSeciton.tsx
@@ -0,0 +1,26 @@
+import SmallProjectCard from '@/components/ui/SmallProjectCard';
+import styles from './ProjectShowcaseSection.module.scss';
+import { personalProjects } from '@/config/projects';
+
+export default function ProjectShowcaseSection() {
+  return (
+    <section className={`${styles.wrapper} nwt--width nwt--flex-n-n-col`}>
+      <div className={styles.header}>
+        <h2 className='nwt--f-h2'>
+          <span className='nwt--txt-gradient'>Project Showcase</span>
+        </h2>
+        <p>
+          High scale Projects with great performance, design and architecture.
+          Discover what I have been working on, and read up on the Case Studies
+          of my Projects as well!
+        </p>
+      </div>
+      <ul className={`${styles.projectList} nwt--ul-none`}>
+        {personalProjects.length > 0 &&
+          personalProjects.map((project, index) => (
+            <li key={index}>{/* <SmallProjectCard project={project} /> */}</li>
+          ))}
+      </ul>
+    </section>
+  );
+}
