6e0a918c096bf87167c98d05fea95031149bbb80
OneDev49 <dev49.sgra@protonmail.com>
2025-09-21 22:51:59 +0200

    feat(mainpage): create individual hero section for mainpage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,13 @@
-import CaretRightIcon from '@/components/icons/ui/CaretRightIcon';
-import HeroSection from '@/components/sections/HeroSection';
 import IntroductionSection from './_components/IntroductionSection';
 import ArticleAdvertisementSection from './_components/ArticleAdvertisementSection';
 import ProjectShowcaseSection from './_components/ProjectShowcaseSection';
 import CTASection from './_components/CTASection';
+import MainpageHeroSection from './_components/MainpageHeroSection';
 
 export default function HomePage() {
   return (
     <main>
-      <HeroSection
-        heading='Stefan Lüllmann'
-        subheading='Full-Stack Software Developer'
-        multiThinWave={1}
-        paragraph='I build performant and scalable web applications with a focus on the Next.js, TypeScript, and Prisma ecosystem.'
-        gradientButton={[
-          {
-            href: '/projects',
-            children: (
-              <>
-                <span>View My Work</span>
-                <CaretRightIcon width={16} height={24} />
-              </>
-            ),
-          },
-        ]}
-      />
+      <MainpageHeroSection />
       <IntroductionSection />
       <ArticleAdvertisementSection />
       <ProjectShowcaseSection />
