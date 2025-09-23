544152aeeb6fd570db5b04309e69f0c9ac58cac0
OneDev49 <dev49.sgra@protonmail.com>
2025-09-23 17:35:03 +0200

    feat: add metadata and projects section to page.tsx

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,9 +1,31 @@
-import ProjectHeroSection from './_components/ProjectsHeroSection';
+import { Metadata } from 'next';
+import ProjectsHeroSection from './_components/ProjectsHeroSection';
+import ProjectsMainSection from './_components/ProjectsMainSection';
+
+/* ADD METADATA HERE */
+export const metadata: Metadata = {
+  title: 'Project Showcase',
+  description:
+    'Explore both Personal and Client Projects made by me. Designed, Developed and deployed entirely by myself.',
+
+  openGraph: {
+    title: 'Project Showcase',
+    description:
+      'Explore every personal and client project I designed, developed and deployed.',
+  },
+
+  twitter: {
+    title: 'Project Showcase',
+    description:
+      'Explore every personal and client project I designed, developed and deployed.',
+  },
+};
 
 export default function ProjectsPage() {
   return (
     <main>
-      <ProjectHeroSection />
+      <ProjectsHeroSection />
+      <ProjectsMainSection />
     </main>
   );
 }
