d875e0465f08a59aa1ef4825a1c9cb4c6207dd13
OneDev49 <dev49.sgra@protonmail.com>
2025-09-25 10:07:20 +0200

    feat: add new CTA section to pages. Remove old About CTA

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,6 +1,8 @@
 import { Metadata } from 'next';
 import ProjectsHeroSection from './_components/ProjectsHeroSection';
 import ProjectsMainSection from './_components/ProjectsMainSection';
+import PersonalImageCTA from '@/components/sections/PersonalImageCTA';
+import GradientButton from '@/components/ui/GradientButton';
 
 /* ADD METADATA HERE */
 export const metadata: Metadata = {
@@ -26,6 +28,11 @@ export default function ProjectsPage() {
     <main>
       <ProjectsHeroSection />
       <ProjectsMainSection />
+      <PersonalImageCTA
+        heading='Get In Touch!'
+        paragraph='Interested in my Projects or want to learn more about me? Reach out to me! I am currently looking for a full-time position as a Web Developer. So if you think that my skills and my mindset aligns well with you or your team, hit me up! I am always happy to have a chat.'
+        getInTouchButton={true}
+      />
     </main>
   );
 }
