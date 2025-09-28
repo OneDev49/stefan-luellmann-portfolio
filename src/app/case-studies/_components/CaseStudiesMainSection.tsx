5fa2d03763bb6d50372a5983bdb6038aa3d36622
OneDev49 <dev49.sgra@protonmail.com>
2025-09-28 19:01:21 +0200

    feat: upgrade GradientButtons to include mode prop

--- src/app/case-studies/_components/CaseStudiesMainSection.tsx
+++ src/app/case-studies/_components/CaseStudiesMainSection.tsx
@@ -11,36 +11,42 @@ export default function CaseStudiesMainSection() {
     <section className={styles.wrapper}>
       {allProjects && (
         <ul className={styles.gridContainer}>
-          {allProjects.map(
-            (project, index) =>
-              project.caseStudyStatus === 'released' && (
-                <li key={index} className={styles.gridItem}>
-                  <div className={styles.gridItemTop}>
-                    <h2 className='nwt--f-h3-small'>
-                      Case Study: {project.title}
-                    </h2>
-                    <ul>
-                      {project.techStack.map((tech) => (
-                        <li key={tech}>
-                          <TechnologyContainer
-                            technology={tech}
-                            variant='small'
-                          />
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                  <p>{project.caseStudyBrief}</p>
-                  <GradientButton
-                    href={`/case-studies/${project.slug}`}
-                    variant='blue'
-                  >
-                    Read {project.title} Case Study
-                    <CaretRightIcon />
-                  </GradientButton>
-                </li>
-              )
-          )}
+          {allProjects.map((project, index) => (
+            <li key={index} className={styles.gridItem}>
+              <div className={styles.gridItemTop}>
+                <h2 className='nwt--f-h3-small'>Case Study: {project.title}</h2>
+                <ul>
+                  {project.techStack.map((tech) => (
+                    <li key={tech}>
+                      <TechnologyContainer technology={tech} variant='small' />
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <p>{project.caseStudyBrief}</p>
+              {project.caseStudyStatus === 'released' ? (
+                <GradientButton
+                  href={`/case-studies/${project.slug}`}
+                  variant='blue'
+                >
+                  Read {project.title} Case Study
+                  <CaretRightIcon />
+                </GradientButton>
+              ) : (
+                <GradientButton
+                  as='button'
+                  type='button'
+                  variant='blue'
+                  animation='none'
+                  mode='disabled'
+                  title='Project Case Study not yet released'
+                  aria-label='Project Case Study not yet released'
+                >
+                  Case Study for this Project is not yet released
+                </GradientButton>
+              )}
+            </li>
+          ))}
         </ul>
       )}
     </section>
