929d09e32306bd0b7ce5367c2ddc861ab3715bab
OneDev49 <dev49.sgra@protonmail.com>
2025-10-17 23:40:38 +0200

    fix: adjust wrong site url format

--- src/config/siteData.ts
+++ src/config/siteData.ts
@@ -1,7 +1,7 @@
 export const siteData = {
   email: 'hallo@stefan-luellmann.com',
   phone: '+491632155075',
-  url: 'https://stefan-luellmann.com/',
+  url: 'https://stefan-luellmann.com',
   social: {
     github: 'https://github.com/OneDev49/',
     linkedin: 'https://www.linkedin.com/in/stefan-l%C3%BCllmann/',
