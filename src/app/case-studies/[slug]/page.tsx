28223d0cea6f276f817493ae6e62f4ddc441ca1c
OneDev49 <dev49.sgra@protonmail.com>
2025-11-16 23:54:06 +0100

    feat: add new mdx components and style to page

--- src/app/case-studies/[slug]/page.tsx
+++ src/app/case-studies/[slug]/page.tsx
@@ -1,12 +1,18 @@
-import { getMdxContent } from '@/lib/mdx';
+import { getMdxContent, Heading, slugify } from '@/lib/mdx';
 import { personalProjects, clientProjects } from '@/config/projects';
 import { notFound } from 'next/navigation';
 import { siteData } from '@/config/siteData';
+import { MDXRemote } from 'next-mdx-remote/rsc';
 
 import TechnologyContainer from '@/components/ui/TechnologyContainer';
 import GradientButton from '@/components/ui/GradientButton';
 import CaretRightIcon from '@/components/icons/ui/CaretRightIcon';
 import styles from './CaseStudy.module.scss';
+import TableOfContent from '@/components/mdx/TableOfContent';
+import TextBlock from '@/components/mdx/TextBlock';
+import ImageCarousel from '@/components/ui/ImageCarousel';
+import MobileTableOfContent from '@/components/mdx/MobileTableOfContent';
+import ShareButton from '@/components/ui/ShareButton';
 
 interface CaseStudyPageProps {
   params: { slug: string };
@@ -43,23 +49,96 @@ export async function generateMetadata({ params }: CaseStudyPageProps) {
   };
 }
 
+const H2 = ({ children }: { children?: React.ReactNode }) => {
+  if (typeof children !== 'string') return <h2>{children}</h2>;
+  const slug = slugify(children);
+  return (
+    <h2 id={slug} className={styles.caseStudyHeading}>
+      {children}
+    </h2>
+  );
+};
+
+const H3 = ({ children }: { children?: React.ReactNode }) => {
+  if (typeof children !== 'string') return <h3>{children}</h3>;
+  const slug = slugify(children);
+  return (
+    <h3 id={slug} className={styles.caseStudyHeading}>
+      {children}
+    </h3>
+  );
+};
+
+const P = (props: React.HTMLAttributes<HTMLParagraphElement>) => (
+  <p className={styles.caseStudyParagraph} {...props} />
+);
+
+const mdxComponents = {
+  TextBlock,
+  h2: H2,
+  h3: H3,
+  p: P,
+};
+
 export default async function CaseStudyPage({ params }: CaseStudyPageProps) {
   const { slug } = params;
-
-  const { content, frontmatter } =
-    (await getMdxContent(slug, 'case-studies')) ?? notFound();
+  const mdxResult = (await getMdxContent(slug, 'case-studies')) ?? notFound();
+  const { frontmatter, headings, content } = mdxResult;
 
   const allProjects = [...personalProjects, ...clientProjects];
   const project = allProjects.find((p) => p.slug === slug);
   if (!project) notFound();
 
+  const projectThumbnailHeading: Heading = {
+    level: 2,
+    text: 'Project Images',
+    slug: 'project-images',
+  };
+
+  const studyOverviewHeading: Heading = {
+    level: 2,
+    text: 'Study Overview',
+    slug: 'study-overview',
+  };
+
+  const extendedHeadings: Heading[] = [
+    projectThumbnailHeading,
+    studyOverviewHeading,
+    ...headings,
+  ];
+
   return (
     <article className={styles.wrapper}>
-      <h1 className='nwt--f-h1'>Case Study: {project.title}</h1>
+      <div className={styles.mainHeading}>
+        <h1 className='nwt--f-h1'>
+          <span className='nwt--txt-gradient'>Case Study: {project.title}</span>
+        </h1>
+        <div>
+          <MobileTableOfContent headings={extendedHeadings} />
+        </div>
+      </div>
       <div className={styles.contentWrapper}>
         <div className={styles.content}>
+          {project.thumbnail && (
+            <div className={styles.thumbnailOverview}>
+              <div>
+                <h2 id='project-images' className='nwt--f-h2'>
+                  Project Images
+                </h2>
+                <hr />
+              </div>
+              <div>
+                <ImageCarousel project={project} bigImageSize='75vw' />
+              </div>
+            </div>
+          )}
           <div className={styles.studyOverview}>
-            <h2 className='nwt--f-h2'>Study Overview</h2>
+            <div>
+              <h2 id='study-overview' className='nwt--f-h2'>
+                Study Overview
+              </h2>
+              <hr />
+            </div>
             <div className={styles.details}>
               <div>
                 <strong>Study Details:</strong>
@@ -143,12 +222,17 @@ export default async function CaseStudyPage({ params }: CaseStudyPageProps) {
                   GitHub
                   <CaretRightIcon />
                 </GradientButton>
+                <ShareButton />
               </div>
             </div>
           </div>
-          <div className={styles.article}>{content}</div>
+          <div className={styles.article}>
+            <MDXRemote source={content} components={mdxComponents} />
+          </div>
         </div>
-        <aside className={styles.tableOfContent}></aside>
+        <aside className={styles.tableOfContent}>
+          <TableOfContent headings={extendedHeadings} variant='sidebar' />
+        </aside>
       </div>
     </article>
   );
