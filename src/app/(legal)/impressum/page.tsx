70a702ce3d77d8a4450dd907e4daf30dd771c3e8
OneDev49 <dev49.sgra@protonmail.com>
2025-11-26 23:48:03 +0100

    fix: fix wrong display of legal pages

--- src/app/(legal)/impressum/page.tsx
+++ src/app/(legal)/impressum/page.tsx
@@ -1,26 +1,25 @@
 import { getMdxContent } from '@/lib/mdx';
+import { MDXRemote } from 'next-mdx-remote/rsc';
 import { notFound } from 'next/navigation';
 
 export async function generateMetadata() {
   const result = await getMdxContent('impressum', 'legal');
-
-  if (!result) {
-    return {};
-  }
+  if (!result) return {};
 
   return {
     title: 'Impressum',
     description:
-      'Impressum von stefan-luellmann.com - Meiner professionellen Portfolio Website.',
+      'Impressum von stefan-luellmann.com - Meiner persönlichen Website.',
   };
 }
 
 export default async function ImpressumPage() {
-  const result = await getMdxContent('impressum', 'legal');
+  const mdxResult = (await getMdxContent('impressum', 'legal')) ?? notFound();
+  const { content } = mdxResult;
 
-  if (!result) {
-    notFound();
-  }
-
-  return <div>{result.content}</div>;
+  return (
+    <div>
+      <MDXRemote source={content} />
+    </div>
+  );
 }
