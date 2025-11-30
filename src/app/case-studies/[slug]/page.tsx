42f5ab04dafed915928192f99013a786bebc8178
OneDev49 <dev49.sgra@protonmail.com>
2025-11-30 15:47:15 +0100

    feat: add twitter metadata to page and remove mdxComponents from page

--- src/app/case-studies/[slug]/page.tsx
+++ src/app/case-studies/[slug]/page.tsx
@@ -1,20 +1,18 @@
-import { getMdxContent, Heading, slugify } from '@/lib/mdx';
+import { getMdxContent, Heading } from '@/lib/mdx';
 import { personalProjects, clientProjects } from '@/config/projects';
 import { notFound } from 'next/navigation';
 import { siteData } from '@/config/siteData';
 import { MDXRemote } from 'next-mdx-remote/rsc';
+import { coreMdxComponents } from '@/components/mdx/mdxArticleComponents/mdxParentFile';
 
 import TechnologyContainer from '@/components/ui/TechnologyContainer';
 import GradientButton from '@/components/ui/GradientButton';
 import CaretRightIcon from '@/components/icons/ui/CaretRightIcon';
 import styles from './CaseStudy.module.scss';
 import TableOfContent from '@/components/mdx/TableOfContent';
-import TextBlock from '@/components/mdx/TextBlock';
-import ImageCarousel from '@/components/ui/ImageCarousel';
+import ImageCarousel from '@/components/ui/ProjectImageCarousel';
 import MobileTableOfContent from '@/components/mdx/MobileTableOfContent';
 import ShareButton from '@/components/ui/ShareButton';
-import Highlight from '@/components/mdx/Highlight';
-import TextSpacer from '@/components/mdx/TextSpacer';
 
 interface CaseStudyPageProps {
   params: { slug: string };
@@ -36,58 +34,33 @@ export async function generateMetadata({ params }: CaseStudyPageProps) {
   const allProjects = [...personalProjects, ...clientProjects];
   const project = allProjects.find((p) => p.slug === slug);
 
+  const DESCRIPTION_TEXT =
+    project?.caseStudyBrief || 'A detailed case study from Stefan Lüllmann';
+
+  const THUMBNAIL = project?.thumbnail
+    ? [{ url: `${siteData.uploadThingUrl}/${project?.thumbnail}` }]
+    : [];
+
   return {
     title: `Case Study: ${project?.title}`,
-    description: project?.caseStudyBrief,
+    description: DESCRIPTION_TEXT,
     authors: [{ name: frontmatter.author }],
+
     openGraph: {
       title: `Case Study: ${project?.title}`,
-      description: project?.caseStudyBrief,
+      description: DESCRIPTION_TEXT,
       url: `/case-studies/${slug}`,
-      images: project?.thumbnail
-        ? [{ url: `${siteData.uploadThingUrl}/${project?.thumbnail}` }]
-        : [],
+      images: THUMBNAIL,
+    },
+
+    twitter: {
+      card: 'summary_large_image',
+      title: `Case Study: ${project?.title}`,
+      description: DESCRIPTION_TEXT,
+      images: THUMBNAIL,
     },
   };
 }
-
-const H2 = ({ children }: { children?: React.ReactNode }) => {
-  if (typeof children !== 'string') return <h2>{children}</h2>;
-  const slug = slugify(children);
-  return (
-    <h2 id={slug} className={styles.caseStudyHeading}>
-      {children}
-    </h2>
-  );
-};
-
-const H3 = ({ children }: { children?: React.ReactNode }) => {
-  if (typeof children !== 'string') return <h3>{children}</h3>;
-  const slug = slugify(children);
-  return (
-    <h3 id={slug} className={styles.caseStudyHeading}>
-      {children}
-    </h3>
-  );
-};
-
-const P = (props: React.HTMLAttributes<HTMLParagraphElement>) => (
-  <p className={styles.caseStudyParagraph} {...props} />
-);
-
-const UL = (props: React.HTMLAttributes<HTMLUListElement>) => (
-  <ul className={styles.caseStudyUnorderedList} {...props} />
-);
-
-const mdxComponents = {
-  TextSpacer,
-  TextBlock,
-  Highlight,
-  h2: H2,
-  h3: H3,
-  p: P,
-  ul: UL,
-};
 
 export default async function CaseStudyPage({ params }: CaseStudyPageProps) {
   const { slug } = params;
@@ -236,7 +209,7 @@ export default async function CaseStudyPage({ params }: CaseStudyPageProps) {
             </div>
           </div>
           <div className={styles.article}>
-            <MDXRemote source={content} components={mdxComponents} />
+            <MDXRemote source={content} components={coreMdxComponents} />
           </div>
         </div>
         <aside className={styles.tableOfContent}>
