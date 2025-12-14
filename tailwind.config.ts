8f197b43e54e6a06af1744afdcf38d45edab99f8
OneDev49 <dev49.sgra@protonmail.com>
2025-12-14 20:11:55 +0100

    feat: add custom breakpoints and animations to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,13 @@ const config: Config = {
     './src/lib/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      sm: '576px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200px',
+      '2xl': '1400px',
+    },
     extend: {
       colors: {
         background: '#00092e',
@@ -27,12 +34,32 @@ const config: Config = {
           '0%': { backgroundPosition: '200% center' },
           '100%': { backgroundPosition: '-200% center' },
         },
+        'sideNav-collapse': {
+          from: { zIndex: '100', transform: 'translateX(0px)' },
+          to: { transform: 'translateX(-380px)' },
+        },
+        'sideNav-expand': {
+          from: { zIndex: '100', transform: 'translateX(-380px)' },
+          to: { transform: 'translateX(0px)' },
+        },
+        'opacity-show': {
+          from: { opacity: '0' },
+          to: { opacity: '1' },
+        },
+        'opacity-hide': {
+          from: { opacity: '1' },
+          to: { opacity: '0' },
+        },
       },
       animation: {
         'custom-text-shine': 'text-shine 4s linear infinite',
+        'opacity-show': 'opacity-show 0.3s forwards',
+        'opacity-hide': 'opacity-hide 0.3s forwards',
+        'sideNav-collapse': 'sideNav-collapse 0.3s forwards',
+        'sideNav-expand': 'sideNav-expand 0.3s forwards',
       },
       backgroundImage: {
-        'gradient-card': 'linear-gradient(180deg, #fff 50%, #929292 100%',
+        'gradient-card': 'linear-gradient(180deg, #fff 50%, #929292 100%)',
       },
       fontFamily: {
         heading: 'var(--font-heading)',
