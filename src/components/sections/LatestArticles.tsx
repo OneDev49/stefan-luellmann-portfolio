556d7aca2b0d661b0a605efd9564d52050f81424
OneDev49 <dev49.sgra@protonmail.com>
2025-12-20 04:11:40 +0100

    feat: create component for latestArticles

---
+++ src/components/sections/LatestArticles.tsx
@@ -0,0 +1,30 @@
+import { getLatestArticles } from '@/lib/mdx/articles';
+
+import ArticleCard from '@/components/ui/ArticleCard';
+
+interface LatestArticlesProps {
+  heading: string;
+  articleAmount?: number;
+}
+
+export default async function LatestArticles({
+  heading,
+  articleAmount = 2,
+}: LatestArticlesProps) {
+  const latestArticles = await getLatestArticles(articleAmount);
+
+  return (
+    <section className='max-w-6xl w-full mx-auto px-4 space-y-10 mb-36'>
+      <h2 className='text-h2 text-transparent font-extrabold capitalize text-center'>
+        <span className='bg-gradient-card bg-clip-text'>{heading}</span>
+      </h2>
+      <ul className='flex justify-center flex-col items-center md:items-start md:flex-row gap-8'>
+        {latestArticles.map((article, index) => (
+          <li key={index}>
+            <ArticleCard post={article} />
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
