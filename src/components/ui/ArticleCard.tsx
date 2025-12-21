eb1326ff5db25a2c432954a00b6eb75d5922b6c9
OneDev49 <dev49.sgra@protonmail.com>
2025-12-21 22:34:35 +0100

    feat: rework ArticleCard component

--- src/components/ui/ArticleCard.tsx
+++ src/components/ui/ArticleCard.tsx
@@ -1,29 +1,32 @@
-import { BlogPost } from '@/lib/mdx';
 import { siteData } from '@/config/siteData';
+import { Article } from '@/lib/mdx/articles';
 
 import Link from 'next/link';
-import styles from './ArticleCard.module.scss';
 import ImageSkeletonLoader from './ImageSkeletonLoader';
 import ArrowRightIcon from '../icons/ui/ArrowRightIcon';
 import FormattedDate from './FormattedDate';
 
 interface ArticleCardProps {
-  post: BlogPost;
+  post: Article;
 }
 
 export default function ArticleCard({ post }: ArticleCardProps) {
   const properCategory =
-    post.category.charAt(0).toUpperCase() + post.category.slice(1);
+    post.topic.charAt(0).toUpperCase() + post.topic.slice(1);
 
   return (
-    <Link href={`/blog/${post.slug}`} className={styles.container}>
-      <article>
+    <Link
+      href={`/blog/${post.slug}`}
+      className='bg-gradient-to-tr from-[#0077ff] to-[#000dff] block text-white rounded-2xl shadow-[0_0_15px_5px_#0051ff] overflow-hidden border-[#0059ff] border transition-all duration-300 ease-in-out max-w-lg hover:shadow-[0_0_15px_5px_#5289ff]'
+    >
+      <article className='flex flex-col'>
         {post.frontmatter.thumbnail && (
-          <div className={styles.imageContainer}>
-            <div className={styles.imageWrapper}>
+          <div className='p-4'>
+            <div className='bg-black overflow-hidden rounded-2xl'>
               <ImageSkeletonLoader
                 src={`${siteData.uploadThingUrl}/${post.frontmatter.thumbnail}`}
                 alt={post.frontmatter.title}
+                className='h-auto object-cover'
                 height={252}
                 width={480}
                 sizes='(min-width: 900px) 25vw, (min-width: 576px) 50vw, 100vw'
@@ -31,18 +34,20 @@ export default function ArticleCard({ post }: ArticleCardProps) {
             </div>
           </div>
         )}
-        <div className={styles.contentContainer}>
-          <div className={styles.misc}>
-            {post.category && <span>Category: {properCategory} | </span>}
+        <div className='flex flex-col gap-4 p-4 bg-[#000830]'>
+          <div className='text-[#9f9f9f] font-extrabold text-[14px]'>
+            {post.topic && <span>Category: {properCategory} | </span>}
             {post.frontmatter.published && (
               <span>
                 <FormattedDate dateString={post.frontmatter.published} />
               </span>
             )}
           </div>
-          <h2 className='nwt--f-h4'>{post.frontmatter.title}</h2>
+          <h2 className='text-[22px] font-extrabold'>
+            {post.frontmatter.title}
+          </h2>
           {post.frontmatter.excerpt && <p>{post.frontmatter.excerpt}</p>}
-          <div className={styles.navLink}>
+          <div className='italic flex items-center font-extrabold gap-1 underline'>
             <span>To the {properCategory} Article</span>
 
             <ArrowRightIcon height={16} width={16} />
