b8b2072688601800ddb5faef924919d6f80034a5
OneDev49 <dev49.sgra@protonmail.com>
2025-12-31 07:56:06 +0100

    feat: add interactiveGrid to latestArticle section

--- src/components/sections/LatestArticles.tsx
+++ src/components/sections/LatestArticles.tsx
@@ -1,4 +1,5 @@
 import { getLatestArticles } from '@/lib/mdx/articles';
+import { InteractiveGrid } from '../effects/InteractiveGrid';
 
 import ArticleCard from '@/components/ui/ArticleCard';
 
@@ -14,17 +15,25 @@ export default async function LatestArticles({
   const latestArticles = await getLatestArticles(articleAmount);
 
   return (
-    <section className='max-w-6xl w-full mx-auto px-4 space-y-10 mb-36'>
-      <h2 className='text-h2 text-transparent font-extrabold capitalize text-center'>
-        <span className='bg-gradient-card bg-clip-text'>{heading}</span>
-      </h2>
-      <ul className='flex justify-center flex-col items-center md:items-start md:flex-row gap-8'>
-        {latestArticles.map((article, index) => (
-          <li key={index}>
-            <ArticleCard post={article} />
-          </li>
-        ))}
-      </ul>
+    <section className='relative'>
+      <div
+        className='absolute inset-0
+                [mask-image:radial-gradient(100%_200px_at_100%_60%,black_30%,transparent_100%),radial-gradient(200px_300px_at_100%_50%,black_30%,transparent_100%),radial-gradient(200px_300px_at_0%_50%,black_30%,transparent_100%)] hidden lg:block'
+      >
+        <InteractiveGrid />
+      </div>
+      <div className='relative z-10 max-w-6xl w-full mx-auto px-4 space-y-10 mb-36'>
+        <h2 className='text-h2 text-transparent font-extrabold capitalize text-center'>
+          <span className='bg-gradient-card bg-clip-text'>{heading}</span>
+        </h2>
+        <ul className='flex justify-center flex-col items-center md:items-start md:flex-row gap-8'>
+          {latestArticles.map((article, index) => (
+            <li key={index}>
+              <ArticleCard post={article} />
+            </li>
+          ))}
+        </ul>
+      </div>
     </section>
   );
 }
